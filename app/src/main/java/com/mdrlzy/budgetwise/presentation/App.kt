500806a93ca7f00a03ba1e05bc929211312f194f
mdrlzy <mordeniusss@gmail.com>
2025-07-19 07:54:33 +0600

    Launch transactions sync on network available

--- app/src/main/java/com/mdrlzy/budgetwise/presentation/App.kt
+++ app/src/main/java/com/mdrlzy/budgetwise/presentation/App.kt
@@ -1,6 +1,7 @@
 package com.mdrlzy.budgetwise.presentation
 
 import android.app.Application
+import android.util.Log
 import androidx.work.Configuration
 import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
@@ -12,8 +13,6 @@ import com.mdrlzy.budgetwise.core.di.CoreComponent
 import com.mdrlzy.budgetwise.core.di.CoreComponentProvider
 import com.mdrlzy.budgetwise.core.domain.BuildConfigFields
 import com.mdrlzy.budgetwise.di.DaggerAppComponent
-import com.mdrlzy.budgetwise.feature.categories.api.CategoryRepo
-import com.mdrlzy.budgetwise.feature.categories.api.di.CategoriesFeatureApi
 import com.mdrlzy.budgetwise.feature.categories.api.di.CategoriesFeatureApiProvider
 import com.mdrlzy.budgetwise.feature.categories.impl.di.CategoriesComponentHolder
 import com.mdrlzy.budgetwise.feature.transactions.impl.di.TransactionsComponentHolder
@@ -21,6 +20,9 @@ import com.mdrlzy.budgetwise.feature.transactions.impl.presentation.worker.SyncT
 import com.mdrlzy.budgetwise.presentation.worker.AppWorkerFactory
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
@@ -41,7 +43,16 @@ class App :
         appIOScope.launch {
             TransactionsComponentHolder.provide(this@App).syncTransactionsUseCase().invoke()
         }
+        setupSyncOnNetworkAvailable()
         initWorker()
+    }
+
+    private fun setupSyncOnNetworkAvailable() {
+        component.networkStatus().onlineStatus.drop(1).onEach { online ->
+            if (online) {
+                TransactionsComponentHolder.provide(this@App).syncTransactionsUseCase().invoke()
+            }
+        }.launchIn(appIOScope)
     }
 
     private fun initWorker() {
